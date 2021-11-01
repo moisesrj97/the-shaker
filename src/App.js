d3f787f9e66fe2faf9b47922150a4d2f1434bddf
jferrergil <jferrergil1@gmail.com>
2021-11-01 11:51:14 +0100

    add navbar without styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.scss';
-import { Route, Switch } from 'react-router';
+import logo from "./logo.svg";
+import "./App.scss";
+import { Route, Switch } from "react-router";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
-    <div className='App'>
-      <div>Header</div>
+    <div className="App">
+      <NavBar />
       <main>
         <Switch>
-          <Route exact path='/'></Route>
+          <Route exact path="/"></Route>
         </Switch>
       </main>
       <div>Footer</div>
