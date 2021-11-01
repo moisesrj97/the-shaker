020d268a70154d3335702c06e74ca58a468258a3
Miguel <miguel.sanchez.goyes@hotmail.comgit add test.jsgit statusgit statusgit commit -m hola>
2021-11-01 13:10:53 +0100

    footer test fixed

--- src/components/footer/Footer.test.js
+++ src/components/footer/Footer.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "../../App";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
 
