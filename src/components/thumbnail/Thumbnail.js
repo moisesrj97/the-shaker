c3c587159c2cc31c91c11a52d7fdaf805482f792
jferrergil <jferrergil1@gmail.com>
2021-11-01 18:12:45 +0100

    add thumbnail whit style

--- src/components/thumbnail/Thumbnail.js
+++ src/components/thumbnail/Thumbnail.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const Thumbnail = () => {
-  return <div>Thumbnail</div>;
+import "./Thumnail.scss";
+
+const Thumbnail = (props) => {
+  return (
+    <div className="thumbnail">
+      <img
+        className="thumbnail__img"
+        src={props.sampleData.strDrinkThumb}
+        alt={props.sampleData.strDrink}
+      />
+      <p className="thumbnail__img-h2">{props.sampleData.strDrink}</p>
+    </div>
+  );
 };
 
 export default Thumbnail;
