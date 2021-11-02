561d887297a06cb8e105f9dc36c3e66d56cc390b
jferrergil <jferrergil1@gmail.com>
2021-11-02 11:05:04 +0100

    add test thumbnail and thumbnailGallery

---
+++ src/components/thumbnail/Thumbnail.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import Thumbnail from './Thumbnail';
+
+test('renders Thumbnail', () => {
+  const history = createMemoryHistory();
+  history.push('/');
+
+  render(
+    <Router history={history}>
+      <Thumbnail
+        sampleData={{
+          strDrink: 'Radioactive Long Island Iced Tea',
+          strDrinkThumb:
+            'https://www.thecocktaildb.com/images/media/drink/rdvqmh1503563512.jpg',
+          idDrink: '16984',
+        }}
+      />
+    </Router>
+  );
+
+  expect(
+    screen.getByText(/Radioactive Long Island Iced Tea/i)
+  ).toBeInTheDocument();
+});
