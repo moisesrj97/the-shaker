e894c58135ce33be329872d82edb7c686278907d
Moisés Rodríguez <65286072+moisesrj97@users.noreply.github.com>
2021-11-02 16:42:14 +0100

    add form styles

--- src/components/searchForm/SearchForm.js
+++ src/components/searchForm/SearchForm.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { DataContext } from '../../context/DataContext';
 
+import './SearchForm.scss';
+
 const SearchForm = (props) => {
   const alpha = Array.from(Array(26)).map((e, i) => i + 65);
   const alphabet = alpha.map((x) => String.fromCharCode(x));
@@ -22,153 +24,171 @@ const SearchForm = (props) => {
 
   const handleSearch = () => props.searchCocktails(state.type, state.query);
 
+  const handleDropdowns = (evt) => {
+    const allInputs = [
+      ...document.querySelectorAll('input'),
+      ...document.querySelectorAll('select'),
+    ];
+
+    allInputs.forEach((e) => {
+      if (e.name !== evt.target.htmlFor) {
+        e.classList.add('hidden');
+      } else {
+        e.classList.remove('hidden');
+      }
+    });
+  };
+
   return (
-    <div>
-      <div>
-        <label className="form__label" htmlFor="byName">
-          By name:
-        </label>
-        <input
-          onChange={handleChange}
-          className="form__input-text"
-          type="text"
-          id="byName"
-          name="byName"
-        />
-      </div>
-      <div>
-        <label className="form__label" htmlFor="byLetter">
-          By Letter:
-        </label>
-        <select
-          onChange={handleChange}
-          className="form__input-select"
-          id="byLetter"
-          name="byLetter"
-        >
-          <option defaultValue disabled>
-            Select an option...
-          </option>
-          {alphabet.map((e, index) => {
+    <div className="form">
+      <label className="form__label" htmlFor="byName" onClick={handleDropdowns}>
+        By name:
+      </label>
+      <input
+        onChange={handleChange}
+        className="form__input-text hidden"
+        type="text"
+        id="byName"
+        name="byName"
+      />
+      <label
+        className="form__label"
+        htmlFor="byLetter"
+        onClick={handleDropdowns}
+      >
+        By Letter:
+      </label>
+      <select
+        onChange={handleChange}
+        className="form__input-select hidden"
+        id="byLetter"
+        name="byLetter"
+      >
+        <option defaultValue disabled>
+          Select an option...
+        </option>
+        {alphabet.map((e, index) => {
+          return (
+            <option key={index} value={e}>
+              {e}
+            </option>
+          );
+        })}
+      </select>
+      <label
+        className="form__label"
+        htmlFor="byAlcoholic"
+        onClick={handleDropdowns}
+      >
+        By Alcoholic:
+      </label>
+      <select
+        onChange={handleChange}
+        className="form__input-select hidden"
+        id="byAlcoholic"
+        name="byAlcoholic"
+      >
+        <option disabled>Select an option...</option>
+        {store.lists.alcoholic.map((e, index) => {
+          return (
+            <option key={index} value={e}>
+              {e}
+            </option>
+          );
+        })}
+      </select>
+      <label className="form__label" htmlFor="byType" onClick={handleDropdowns}>
+        By Type:
+      </label>
+      <select
+        onChange={handleChange}
+        className="form__input-select hidden"
+        id="byType"
+        name="byType"
+      >
+        <option defaultValue disabled>
+          Select an option...
+        </option>
+        {store.lists.types.map((e, index) => {
+          return (
+            <option key={index} value={e}>
+              {e}
+            </option>
+          );
+        })}
+      </select>
+      <label
+        className="form__label"
+        htmlFor="byGlass"
+        onClick={handleDropdowns}
+      >
+        By Glass:
+      </label>
+      <select
+        onChange={handleChange}
+        className="form__input-select hidden"
+        id="byGlass"
+        name="byGlass"
+      >
+        <option defaultValue disabled>
+          Select an option...
+        </option>
+        {store.lists.glasses.map((e, index) => {
+          return (
+            <option key={index} value={e}>
+              {e}
+            </option>
+          );
+        })}
+      </select>
+      <label
+        className="form__label"
+        htmlFor="byIngredient"
+        onClick={handleDropdowns}
+      >
+        By Ingredient/s:
+      </label>
+      <select
+        className="form__input-select hidden"
+        id="byIngredient"
+        name="byIngredient"
+        onChange={handleChange}
+      >
+        <option defaultValue disabled>
+          Select up to 3 options...
+        </option>
+        {store.lists.ingredients.sort().map((e, index) => {
+          return (
+            <option key={index} value={e}>
+              {e}
+            </option>
+          );
+        })}
+      </select>
+      <ul class="form__ingredients">
+        {state.type === 'byIngredient' &&
+          state.query.map((e, index) => {
             return (
-              <option key={index} value={e}>
+              <li key={index}>
                 {e}
-              </option>
+                <i
+                  class="far fa-trash-alt"
+                  onClick={() =>
+                    setState({
+                      ...state,
+                      query: state.query.filter(
+                        (i, iIndex) => iIndex !== index
+                      ),
+                    })
+                  }
+                ></i>
+              </li>
             );
           })}
-        </select>
-      </div>
-      <div>
-        <label className="form__label" htmlFor="byAlcoholic">
-          By Alcoholic:
-        </label>
-        <select
-          onChange={handleChange}
-          className="form__input-select"
-          id="byAlcoholic"
-          name="byAlcoholic"
-        >
-          <option disabled>Select an option...</option>
-          {store.lists.alcoholic.map((e, index) => {
-            return (
-              <option key={index} value={e}>
-                {e}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div>
-        <label className="form__label" htmlFor="byType">
-          By Type:
-        </label>
-        <select
-          onChange={handleChange}
-          className="form__input-select"
-          id="byType"
-          name="byType"
-        >
-          <option defaultValue disabled>
-            Select an option...
-          </option>
-          {store.lists.types.map((e, index) => {
-            return (
-              <option key={index} value={e}>
-                {e}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div>
-        <label className="form__label" htmlFor="byGlass">
-          By Glass:
-        </label>
-        <select
-          onChange={handleChange}
-          className="form__input-select"
-          id="byGlass"
-          name="byGlass"
-        >
-          <option defaultValue disabled>
-            Select an option...
-          </option>
-          {store.lists.glasses.map((e, index) => {
-            return (
-              <option key={index} value={e}>
-                {e}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div>
-        <label className="form__label" htmlFor="byIngredient">
-          By Ingredient/s:
-        </label>
-        <select
-          onChange={handleChange}
-          className="form__input-select"
-          id="byIngredient"
-          name="byIngredient"
-          onChange={handleChange}
-        >
-          <option defaultValue disabled>
-            Select up to 3 options...
-          </option>
-          {store.lists.ingredients.sort().map((e, index) => {
-            return (
-              <option key={index} value={e}>
-                {e}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div>
-        <ul>
-          {state.type === 'byIngredient' &&
-            state.query.map((e, index) => {
-              return (
-                <li key={index}>
-                  {e}
-                  <i
-                    class="far fa-trash-alt"
-                    onClick={() =>
-                      setState({
-                        ...state,
-                        query: state.query.filter(
-                          (i, iIndex) => iIndex !== index
-                        ),
-                      })
-                    }
-                  ></i>
-                </li>
-              );
-            })}
-        </ul>
-      </div>
-      <button onClick={handleSearch}>Shake it!</button>
+      </ul>
+      <button onClick={handleSearch} class="form__button">
+        Shake it!
+      </button>
     </div>
   );
 };
