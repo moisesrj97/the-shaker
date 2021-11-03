ae4840d222fa5be670e219ec574041ae8c6d2ccc
Miguel <miguel.sanchez.goyes@hotmail.comgit add test.jsgit statusgit statusgit commit -m hola>
2021-11-03 11:42:53 +0100

    custom methods

--- src/services/UsersAPI.js
+++ src/services/UsersAPI.js
@@ -54,4 +54,38 @@ export default class UsersAPI {
     });
     return response.data;
   }
+  /* ----------------------------------------------- */
+
+  static async createCustom(id, newCustom) {
+    const { data } = await axios.get(baseURL + 'user/' + id);
+    const response = await axios.patch(baseURL + 'user/' + id, {
+      ...data,
+      custom: [...data.custom, { ...newCustom, id: uuid() }],
+    });
+    return response.data;
+  }
+
+  static async updateCustom(id, customId, editCustom) {
+    const { data } = await axios.get(baseURL + 'user/' + id);
+    const response = await axios.patch(baseURL + 'user/' + id, {
+      ...data,
+      custom: data.custom.map((e) => {
+        if (e.id === customId) {
+          return { ...editCustom };
+        } else {
+          return { ...e };
+        }
+      }),
+    });
+    return response.data.custom.find((e) => e.id === customId);
+  }
+
+  static async removeCustom(id, customId) {
+    const { data } = await axios.get(baseURL + 'user/' + id);
+    const response = await axios.patch(baseURL + 'user/' + id, {
+      ...data,
+      custom: data.custom.filter((e) => e.id !== customId),
+    });
+    return response.data;
+  }
 }
