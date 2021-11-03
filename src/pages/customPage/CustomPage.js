4e052ce8cbf65087a65950dd16ab48dce1207394
jferrergil <jferrergil1@gmail.com>
2021-11-03 17:44:29 +0100

    change css button in CustomPage

--- src/pages/customPage/CustomPage.js
+++ src/pages/customPage/CustomPage.js
@@ -10,7 +10,7 @@ const CustomPage = () => {
   const { store } = useContext(DataContext);
 
   return (
-    <div>
+    <div className="add">
       <button
         className="add-button"
         onClick={() => history.push('/create-custom')}
