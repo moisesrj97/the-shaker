439d1a5f79938b4f2f0e73a65ec500d482f09201
Moisés Rodríguez <65286072+moisesrj97@users.noreply.github.com>
2021-11-04 09:50:26 +0100

    fix styles

--- src/pages/formCustom/formCustom.js
+++ src/pages/formCustom/formCustom.js
@@ -78,178 +78,185 @@ const FormCustom = () => {
   return (
     <div className="boxForm">
       <h2 className="favorites">Create Cocktail</h2>
-      <div className="">
-        <label className="form__label" htmlFor="name">
-          Name
-        </label>
-        <input
-          className="form__input-text  form__input-text-custom"
-          type="text"
-          id="name"
-          name="name"
-          onChange={handleChange}
-          value={state.name}
-        />
-      </div>
-      <div>
-        <label className="form__label" htmlFor="thumb">
-          Image URL
-        </label>
-        <input
-          className="form__input-text form__input-text-custom"
-          type="text"
-          id="thumb"
-          name="thumb"
-          onChange={handleChange}
-          value={state.thumb}
-        />
+      <div className="boxForm__sections">
+        <div className="boxForm__sections-section">
+          <div className="">
+            <label className="form__label" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="form__input-text  form__input-text-custom"
+              type="text"
+              id="name"
+              name="name"
+              onChange={handleChange}
+              value={state.name}
+            />
+          </div>
+          <div>
+            <label className="form__label" htmlFor="thumb">
+              Image URL
+            </label>
+            <input
+              className="form__input-text form__input-text-custom"
+              type="text"
+              id="thumb"
+              name="thumb"
+              onChange={handleChange}
+              value={state.thumb}
+            />
+          </div>
+
+          <div>
+            <label className="form__label" htmlFor="recipe">
+              Recipe
+            </label>
+            <textarea
+              rows="7"
+              className="form__input-text form__input-text-custom"
+              id="recipe"
+              name="recipe"
+              onChange={handleChange}
+              value={state.recipe}
+            />
+          </div>
+          <div>
+            <label className="form__label" htmlFor="type">
+              Type:
+            </label>
+            <select
+              className="form__input-select"
+              id="type"
+              name="type"
+              defaultValue="default"
+              onChange={handleChange}
+              value={state.type}
+            >
+              <option hidden value="default">
+                Select an option...
+              </option>
+              {store.lists.types.map((e, index) => {
+                return (
+                  <option key={index} value={e}>
+                    {e}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div>
+            <label className="form__label" htmlFor="glass">
+              Glass:
+            </label>
+            <select
+              className="form__input-select"
+              id="glass"
+              name="glass"
+              defaultValue="default"
+              onChange={handleChange}
+              value={state.glass}
+            >
+              <option hidden value="default">
+                Select an option...
+              </option>
+              {store.lists.glasses.map((e, index) => {
+                return (
+                  <option key={index} value={e}>
+                    {e}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div>
+            <label className="form__label" htmlFor="alcoholic">
+              Alcoholic:
+            </label>
+            <select
+              className="form__input-select"
+              id="alcoholic"
+              name="alcoholic"
+              defaultValue="default"
+              onChange={handleChange}
+              value={state.alcoholic}
+            >
+              <option disabled hidden value="default">
+                Select an option...
+              </option>
+              {store.lists.alcoholic.map((e, index) => {
+                return (
+                  <option key={index} value={e}>
+                    {e}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
+        <div className="boxForm__sections-section">
+          <div>
+            <label className="form__label" htmlFor="ingredientes">
+              Ingredients:
+            </label>
+            <select
+              className="form__input-select"
+              id="ingredientes"
+              name="ingredientes"
+              defaultValue="default"
+              onChange={(evt) => {
+                setIngredients([
+                  ...ingredients,
+                  { name: evt.target.value, amount: '' },
+                ]);
+              }}
+            >
+              <option hidden value="default">
+                Select up to 3 options...
+              </option>
+              {store.lists.ingredients.sort().map((e, index) => {
+                return (
+                  <option key={index} value={e}>
+                    {e}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div>
+            {ingredients.map((e, index) => {
+              if (index > 0) {
+                return (
+                  <div className="form__label-div" key={index}>
+                    <label className="form__label-custom" htmlFor={e.name}>
+                      {e.name}
+                    </label>
+                    <i
+                      className="far fa-trash-alt"
+                      onClick={() =>
+                        setIngredients(
+                          ingredients.filter(
+                            (d, deleteIndex) => deleteIndex !== index
+                          )
+                        )
+                      }
+                    ></i>
+                    <input
+                      placeholder="Add amount"
+                      type="text"
+                      value={e.amount}
+                      onChange={handleIngredientInput}
+                      name={e.name}
+                      className="form__input-text form__input-text-custom"
+                      id={e.name}
+                    />
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </div>
       </div>
 
-      <div>
-        <label className="form__label" htmlFor="recipe">
-          Recipe
-        </label>
-        <input
-          className="form__input-text form__input-text-custom"
-          type="text"
-          id="recipe"
-          name="recipe"
-          onChange={handleChange}
-          value={state.recipe}
-        />
-      </div>
-      <div>
-        <label className="form__label" htmlFor="type">
-          Type:
-        </label>
-        <select
-          className="form__input-select"
-          id="type"
-          name="type"
-          defaultValue="default"
-          onChange={handleChange}
-          value={state.type}
-        >
-          <option hidden value="default">
-            Select an option...
-          </option>
-          {store.lists.types.map((e, index) => {
-            return (
-              <option key={index} value={e}>
-                {e}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div>
-        <label className="form__label" htmlFor="glass">
-          Glass:
-        </label>
-        <select
-          className="form__input-select"
-          id="glass"
-          name="glass"
-          defaultValue="default"
-          onChange={handleChange}
-          value={state.glass}
-        >
-          <option hidden value="default">
-            Select an option...
-          </option>
-          {store.lists.glasses.map((e, index) => {
-            return (
-              <option key={index} value={e}>
-                {e}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div>
-        <label className="form__label" htmlFor="alcoholic">
-          Alcoholic:
-        </label>
-        <select
-          className="form__input-select"
-          id="alcoholic"
-          name="alcoholic"
-          defaultValue="default"
-          onChange={handleChange}
-          value={state.alcoholic}
-        >
-          <option disabled hidden value="default">
-            Select an option...
-          </option>
-          {store.lists.alcoholic.map((e, index) => {
-            return (
-              <option key={index} value={e}>
-                {e}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div>
-        <label className="form__label" htmlFor="ingredientes">
-          Ingredients:
-        </label>
-        <select
-          className="form__input-select"
-          id="ingredientes"
-          name="ingredientes"
-          defaultValue="default"
-          onChange={(evt) => {
-            setIngredients([
-              ...ingredients,
-              { name: evt.target.value, amount: '' },
-            ]);
-          }}
-        >
-          <option hidden value="default">
-            Select up to 3 options...
-          </option>
-          {store.lists.ingredients.sort().map((e, index) => {
-            return (
-              <option key={index} value={e}>
-                {e}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div>
-        {ingredients.map((e, index) => {
-          if (index > 0) {
-            return (
-              <div className="form__label-div" key={index}>
-                <label className="form__label-custom" htmlFor={e.name}>
-                  {e.name}
-                </label>
-                <i
-                  className="far fa-trash-alt"
-                  onClick={() =>
-                    setIngredients(
-                      ingredients.filter(
-                        (d, deleteIndex) => deleteIndex !== index
-                      )
-                    )
-                  }
-                ></i>
-                <input
-                  placeholder="Add amount"
-                  type="text"
-                  value={e.amount}
-                  onChange={handleIngredientInput}
-                  name={e.name}
-                  className="form__input-text form__input-text-custom"
-                  id={e.name}
-                />
-              </div>
-            );
-          }
-        })}
-      </div>
       <button onClick={handleSubmit} className="add-button add-button-custom">
         Add
       </button>
