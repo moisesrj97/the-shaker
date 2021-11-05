fe6784ce0981bfccfcf8ebca399636f8f9557a62
Moisés Rodríguez <65286072+moisesrj97@users.noreply.github.com>
2021-11-05 17:49:30 +0100

    refactor navbar test

--- src/components/navbar/NavBar.test.js
+++ src/components/navbar/NavBar.test.js
@@ -1,8 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import App from '../../App';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import NavBar from './NavBar';
 
 const user = {
   email: 'johndoe@me.com',
@@ -12,71 +11,31 @@ const user = {
 
 jest.mock('@auth0/auth0-react');
 
-describe('Given the component NavBar...', () => {
+describe('Given the component NavBar being logged in...', () => {
+  beforeEach(() => {
+    useAuth0.mockReturnValue({
+      isAuthenticated: true,
+      user,
+      logout: jest.fn(),
+      loginWithRedirect: jest.fn(),
+    });
+
+    render(
+      <Router>
+        <NavBar />
+      </Router>
+    );
+  });
   describe('When component is instantiated...', () => {
     test('renders navbar when your logged in', () => {
-      useAuth0.mockReturnValue({
-        isAuthenticated: true,
-        user,
-        logout: jest.fn(),
-        loginWithRedirect: jest.fn(),
-      });
-
-      const history = createMemoryHistory();
-      history.push('/');
-
-      render(
-        <Router history={history}>
-          <App />
-        </Router>
-      );
-
       expect(screen.getByText('Cocktails')).toBeInTheDocument();
       expect(screen.getByText('My Cocktails')).toBeInTheDocument();
       expect(screen.getByText('Favorites')).toBeInTheDocument();
       expect(screen.getByText(/Logout/i)).toBeInTheDocument();
     });
   });
-  describe('When component is instantiated and you are logged out...', () => {
-    test('renders navbar when you are logged out', () => {
-      useAuth0.mockReturnValue({
-        isAuthenticated: true,
-        user: undefined,
-        logout: jest.fn(),
-        loginWithRedirect: jest.fn(),
-      });
-
-      const history = createMemoryHistory();
-      history.push('/');
-
-      render(
-        <Router history={history}>
-          <App />
-        </Router>
-      );
-
-      expect(screen.getByText('Cocktails')).toBeInTheDocument();
-      expect(screen.getByText(/Login/i)).toBeInTheDocument();
-    });
-  });
   describe('When component is instantiated and you are logged in with mobile...', () => {
     test('renders mobile navbar when you are logged in', () => {
-      useAuth0.mockReturnValue({
-        isAuthenticated: true,
-        user,
-        logout: jest.fn(),
-        loginWithRedirect: jest.fn(),
-      });
-
-      const history = createMemoryHistory();
-      history.push('/');
-
-      render(
-        <Router history={history}>
-          <App />
-        </Router>
-      );
-
       const hamburguer = document.querySelector('.hamburger-react');
 
       fireEvent.click(hamburguer);
@@ -87,29 +46,40 @@ describe('Given the component NavBar...', () => {
       expect(screen.getAllByText(/Logout/i)).toHaveLength(2);
     });
   });
-  describe('When component is instantiated and you are logged out with mobile...', () => {
-    test('renders mobile navbar when you are not logged in', () => {
-      useAuth0.mockReturnValue({
-        isAuthenticated: true,
-        user: undefined,
-        logout: jest.fn(),
-        loginWithRedirect: jest.fn(),
-      });
+});
 
-      const history = createMemoryHistory();
-      history.push('/');
+describe('Given the component NavBar being logged out...', () => {
+  beforeEach(() => {
+    useAuth0.mockReturnValue({
+      isAuthenticated: false,
+      user: null,
+      logout: jest.fn(),
+      loginWithRedirect: jest.fn(),
+    });
 
-      render(
-        <Router history={history}>
-          <App />
-        </Router>
-      );
-
+    render(
+      <Router>
+        <NavBar />
+      </Router>
+    );
+  });
+  describe('When component is instantiated...', () => {
+    test('renders navbar when your logged out', () => {
+      expect(screen.getByText('Cocktails')).toBeInTheDocument();
+      expect(screen.queryByText('My Cocktails')).toBeNull();
+      expect(screen.queryByText('Favorites')).toBeNull();
+      expect(screen.getByText(/Login/i)).toBeInTheDocument();
+    });
+  });
+  describe('When component is instantiated and you are logged in with mobile...', () => {
+    test('renders mobile navbar when you are logged out', () => {
       const hamburguer = document.querySelector('.hamburger-react');
 
       fireEvent.click(hamburguer);
 
       expect(screen.getAllByText('Cocktails')).toHaveLength(2);
+      expect(screen.queryByText('My Cocktails')).toBeNull();
+      expect(screen.queryByText('Favorites')).toBeNull();
       expect(screen.getAllByText(/Login/i)).toHaveLength(2);
     });
   });
