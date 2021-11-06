4c56b1ee182b3ea2de168fe6bb042b1e200876cb
Moisés Rodríguez <65286072+moisesrj97@users.noreply.github.com>
2021-11-06 14:49:14 +0100

    refactor favorites test

--- src/pages/favorites/Favorites.test.js
+++ src/pages/favorites/Favorites.test.js
@@ -1,9 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
+import { BrowserRouter } from 'react-router-dom';
+import Favorites from './Favorites';
 
-import DataContextProvider from '../../context/DataContext';
-import App from '../../App';
+import DataContextProvider, { DataContext } from '../../context/DataContext';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const user = {
@@ -12,6 +11,7 @@ const user = {
   sub: 'google-oauth2|2147627834623744883746',
 };
 jest.mock('@auth0/auth0-react');
+jest.mock('axios');
 
 describe('Given the component Favorites...', () => {
   describe('When component is instantiated...', () => {
@@ -23,15 +23,47 @@ describe('Given the component Favorites...', () => {
         loginWithRedirect: jest.fn(),
       });
 
-      const history = createMemoryHistory();
-      history.push('/favorites');
-
       render(
-        <DataContextProvider>
-          <Router history={history}>
-            <App />
-          </Router>
-        </DataContextProvider>
+        <DataContext.Provider
+          value={{
+            store: {
+              user: {
+                id: 'johndoe@me.com',
+                fav: [
+                  {
+                    id: '',
+                    name: '',
+                    thumb: '',
+                    apiId: '',
+                  },
+                  {
+                    id: '12345',
+                    name: 'mock cocktail',
+                    thumb: 'asdad.jpg',
+                    apiId: '12345',
+                  },
+                ],
+                custom: [
+                  {
+                    name: '',
+                    thumb: '',
+                    recipe: '',
+                    type: '',
+                    glass: '',
+                    alcoholic: '',
+                    ingredientes: [''],
+                    ingredientesAmount: [''],
+                    id: '',
+                  },
+                ],
+              },
+            },
+          }}
+        >
+          <BrowserRouter>
+            <Favorites />
+          </BrowserRouter>
+        </DataContext.Provider>
       );
 
       expect(await screen.findByText(/Mock Cocktail/i)).toBeInTheDocument();
